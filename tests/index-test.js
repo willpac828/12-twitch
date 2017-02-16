5987edb3a894e06ff80090d827f192600330233b
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-16 14:30:52 -0600

    I fixed it!!!

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,8 +2,6 @@
 
 import './exercises/01-create-message-item-test';
 import './exercises/02-add-message-item-test';
-import './exercises/03-sum-from-list-test';
-import './exercises/04-reverse-list-test';
 
 import './twitch/01-create-game-element-test';
 import './twitch/02-add-games-to-list-test';
