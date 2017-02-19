8c72337f8478c6469c1a3446cd9a350dfe4a2df1
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-02-19 17:19:45 -0600

    Passed 2nd test

--- app/exercise/create-message-item.js
+++ app/exercise/create-message-item.js
@@ -1,1 +1,9 @@
-export default null;
+export default function createMessageItem(message) {
+
+  const li = document.createElement('LI');
+
+
+  li.innerHTML = `<span class="message-item__username">${message.username}</span> ${message.message}`;
+
+  return li;
+};
