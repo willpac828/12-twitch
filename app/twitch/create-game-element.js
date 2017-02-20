0288f719a9a24e6cabdbe50c83455c885ec7cee7
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-02-19 22:00:26 -0600

    markup first go

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,13 +1,13 @@
-export default function createGameElement() {
-  tdocument.createElement('DIV');
-  el.classList.add('game-item');
+// export default function createGameElement() {
+//   document.createElement('DIV');
+//   el.classList.add('game-item');
+//
+//   el.innerHTML = `<h3 class="game-item__name">${item.name}</h3>`;
+//   el.innerHTML = `<h4 class="game-item__popularity">${item.popularity}</h4>`;
+//   el.innerHTML = `<img src="item.box.large" class="game-item__pic" alt="Starcraft II">
+//
+//   return el;
 
-  el.innerHTML = `<h3 class="game-item__name">${item.name}</h3>`;
-  el.innerHTML = `<h4 class="game-item__popularity">${item.popularity}</h4>`;
-  el.innerHTML = `<img src="item.box.large" class="game-item__pic" alt="Starcraft II">
 
-  return el;
-
-
-
-};
+//
+// };
